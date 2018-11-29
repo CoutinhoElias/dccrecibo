3a8474092558d0877ed015aeae241ff718a1cbb7
CoutinhoElias <coutinho.elias@gmail.com>
2018-11-29 09:03:09 -0300

    Ajustes nos menus para dispositivos móveis.

--- dccrecibo/core/forms.py
+++ dccrecibo/core/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.forms import inlineformset_factory
 from material import *
 
-from dccrecibo.core.models import Person
+from dccrecibo.core.models import Person, Receipt, ReceiptMovimento
 
 
 class PersonForm(forms.ModelForm):
@@ -12,3 +13,20 @@ class PersonForm(forms.ModelForm):
             'name',
             'cpf_cnpj',
         )
+
+
+class ReceiptForm(forms.models.ModelForm):
+
+    class Meta:
+        model = Receipt
+        fields = '__all__'
+
+    layout = Layout(
+                     Row('person','vehicle'),
+                     Row('chassis', 'color'),
+                     Row('author'),
+                     Row('observation'),
+                    )
+
+ReceiptMovimentoFormSet = inlineformset_factory(Receipt, ReceiptMovimento, can_delete=True,
+        fields=('form_of_payment', 'kind', 'form_of_payment'), extra=1)
--- dccrecibo/core/views.py
+++ dccrecibo/core/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.db import transaction
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from django.template.loader import get_template
@@ -9,7 +10,7 @@ from django.template.loader import get_template
 
 from django.views.generic import View
 
-from dccrecibo.core.forms import PersonForm
+from dccrecibo.core.forms import PersonForm, ReceiptForm, ReceiptMovimentoFormSet
 from dccrecibo.core.models import Receipt, ReceiptMovimento, Person
 from dccrecibo.utils import render_to_pdf# created in step 4
 
@@ -99,3 +100,60 @@ def person_create(request):
 def person_list(request):
     persons = Person.objects.all().order_by("name")
     return render(request, 'person_list.html', {'persons': persons})
+
+
+#----------------------------------------------------------------------------------------------------------------------
+
+def invoices_create(request):
+    success_message = 'The receipt was edited correctly.'
+    if request.method == 'POST':
+        form = ReceiptForm(request.POST)
+        formset = ReceiptMovimentoFormSet(request.POST)
+
+        if form.is_valid() and formset.is_valid():
+            with transaction.atomic():
+                invoice = form.save()
+                formset.instance = invoice
+                formset.save()
+
+            return redirect('/lancamento/pedido/listar/')
+    else:
+        form = ReceiptForm()
+        formset = ReceiptMovimentoFormSet()
+
+    forms = [formset.empty_form] + formset.forms
+    context = {'form': form, 'formset': formset, 'forms': forms}
+    return render(request, 'checkout/invoice_form.html', context)
+
+
+def receipt_update(request, pk):
+    # Pega a chave da URL acima com (request, pk)
+    # joga na variável invoice na linha abaixo passando o modelo MESTRE e osparâmetros que desejo como filtro
+    receipt = get_object_or_404(Receipt, pk=pk)
+    #print(request.method)
+    if request.method == 'POST':
+        # Os formulários InvoiceForm receberá o request.POST com os campos em branco
+        form = ReceiptForm(request.POST, instance=receipt)
+        formset = ReceiptMovimentoFormSet(request.POST, instance=receipt)
+
+        # Valida os formulários MESTRE(InvoiceForm) e DETALHE(ItemFormSet)
+        if form.is_valid() and formset.is_valid():
+            with transaction.atomic():
+                form.save()
+                formset.save()
+
+            return redirect('/lista/')
+    else:
+        #Caso não seja POST ele trará o formulário com as informações preenchidas do parâmetro invoice
+        #que pegamos da URL quandopassamoso request de pk na entrada da função acima.
+        form = ReceiptForm(instance=receipt)
+        formset = ReceiptMovimentoFormSet(instance=receipt)
+    #Passamos os dois forms para uma variável com um nome qualquer (Neste cado usamos o nome "forms" afim de dar a idéia
+    # demais de um formulário conforme abaixo:
+    #Na linha context passamos também os dois contextos e
+    #por fim na linha final passamos o retorno da função onde chamamos o template com o context.
+    forms = [formset.empty_form] + formset.forms
+    context = {'form': form, 'formset': formset, 'forms': forms}
+    return render(request, 'checkout/invoice_form.html', context)
+
+#----------------------------------------------------------------------------------------------------------------------
